e13b51c3a974ab9742c2129eac1b62c7718b3cbb
albertogfv <albertogfv@gmail.com>
2021-03-10 19:24:47 -0800

    dialog/order item/ actiivty layout

--- app/src/main/java/com/albertogfv/snack_truck/ui/orderList/OrderActivity.kt
+++ app/src/main/java/com/albertogfv/snack_truck/ui/orderList/OrderActivity.kt
@@ -7,6 +7,6 @@ import com.albertogfv.snack_truck.R
 class OrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_order)
     }
 }
