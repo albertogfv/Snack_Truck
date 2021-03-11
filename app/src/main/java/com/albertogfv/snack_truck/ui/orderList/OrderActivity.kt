be7467253b9f02aa54153087dcb673c9947bb8e6
albertogfv <albertogfv@gmail.com>
2021-03-10 18:30:26 -0800

    built file structure

---
+++ app/src/main/java/com/albertogfv/snack_truck/ui/orderList/OrderActivity.kt
@@ -0,0 +1,12 @@
+package com.albertogfv.snack_truck.ui.orderList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.albertogfv.snack_truck.R
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
